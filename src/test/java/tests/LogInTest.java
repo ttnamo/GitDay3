feefdace110d0d1ca5c3300c2689149d53c14c35
ttnamo <31378073+ttnamo@users.noreply.github.com>
2019-02-01 22:24:40 -0500

    adding title to login

--- src/test/java/tests/LogInTest.java
+++ src/test/java/tests/LogInTest.java
@@ -25,7 +25,8 @@ public class LogInTest {
         driver.get("e.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
     driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
     driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
-    Assert.assertEquals(driver.getTitle(), "Web Orders");
+        String title=driver.getTitle();
+    Assert.assertEquals(title, "Web Orders");
 }
 
 }
