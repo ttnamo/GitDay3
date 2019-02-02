93780779fc3ea562f8314923ed3cf252969cedfd
ttnamo <ttnamo@gmail.com>
2019-02-01 21:24:53 -0500

    more added to login

--- src/test/java/tests/LogInTest.java
+++ src/test/java/tests/LogInTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -27,7 +28,17 @@ public class LogInTest {
     driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
     Assert.assertEquals(driver.getTitle(), "Web Orders");
 }
-@Test
+    @Test
+    public void loginTest(){
+        driver.get("e.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
+
+        driver.findElement(By.id("ctl00_logout")).click();
+
+        Assert.assertEquals(driver.getTitle(), "Web Orders Login");
+    }
+@AfterMethod
     public void CleanUp(){
         driver.close();
 
